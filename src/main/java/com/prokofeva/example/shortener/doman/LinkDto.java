b619798802bfe46420ecec2e9bb44d6eb05cb74f
MariyaPro <mariaprokof@gmail.com>
2024-02-17 01:37:39 +0300

    edit docker-compose.yaml, DB

--- src/main/java/com/prokofeva/example/shortener/doman/LinkDto.java
+++ src/main/java/com/prokofeva/example/shortener/doman/LinkDto.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
